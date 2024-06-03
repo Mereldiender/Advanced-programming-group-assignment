29d92bd814ea7efbe8798a2002c34cef672bc5c7
RosannevO <167773601+RosannevO@users.noreply.github.com>
2024-06-03 17:11:56 +0200

    Update random_forest.py

--- random_forest.py
+++ random_forest.py
@@ -7,7 +7,8 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.metrics import (
-    accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+    balanced_accuracy_score, confusion_matrix, ConfusionMatrixDisplay,
+    make_scorer, average_precision_score
     )
 
 
@@ -82,13 +83,16 @@ def rf_classifier(train_data, test_data, amount_trees):
     # Predict the Test set results
     y_pred = multi_target_rfc.predict(X_test)
 
-    # Calculate the accuracy score for each target
-    accuracy_scores = [accuracy_score(
-        y_test.iloc[:, i], y_pred[:, i]) for i in range(y_test.shape[1])
+    # Calculate the average precision score for each target
+    precision_scores = [average_precision_score(y_test.iloc[:, i], y_pred[
+        :, i]) for i in range(y_test.shape[1])
         ]
-    avg_accuracy_score = sum(accuracy_scores) / len(accuracy_scores)
+    avg_precision_score = sum(precision_scores) / len(precision_scores)
+    print('Model average precision score after hyperparameter tuning: {0:0.4f}'.format(
+        avg_precision_score)
+        )
 
-    return y_test, y_pred, avg_accuracy_score, multi_target_rfc
+    return y_test, y_pred, avg_precision_score, multi_target_rfc
 
 
 # Creating a seaborn bar plot
@@ -165,12 +169,20 @@ def create_confusion_matrix(y_test, y_pred, labels):
     plt.tight_layout()
     plt.show()
 
+## ---------------- Create scorer for hyperparameter optimization
+def avg_precision(y_true, y_pred):
+    """
+    Compute the average precision for multi-output classification.
+    """
+    precision_scores = [average_precision_score(y_true.iloc[:, i], y_pred[:, i]) for i in range(y_true.shape[1])]
+    return np.mean(precision_scores)
+
 
 ## ---------------- Using random search to optimize random forest
 def hyperparameter_optimization(X_train, X_test, y_train, y_test):
     """
     Returns the parameters for the multi-output classifier that result in 
-    the highest average accuracy score.
+    the highest average balanced accuracy score.
 
     Parameters
     ----------
@@ -191,29 +203,32 @@ def hyperparameter_optimization(X_train, X_test, y_train, y_test):
     best_params : dict
         Dictionary containing the values of the hyperparameters that result
         in the best average accuracy score
-    avg_accuracy_score : float
+    avg_bal_accuracy_score : float
         Average accuracy score over the two variables to be predicted
     """
     # Hyperparameter grid
     param_grid = {
-        'estimator__max_depth': [None] + list(
-            np.linspace(3, 100, num=100).astype(int)),                          # Maximum number of levels in tree
-        'estimator__max_features': ['sqrt', 'log2', None
-                                    ] + list(np.arange(0.1, 1.1, 0.1)),         # Number of features to consider at every split
-        'estimator__max_leaf_nodes': [None] + list(
-            np.linspace(10, 100, num=100).astype(int)),                         # How many leaf nodes can be visited
+        'estimator__max_depth': [None] + list(np.linspace(
+            3, 100, num=100).astype(int)
+            ),                                                                  # Maximum number of levels in tree
+        'estimator__max_features': ['sqrt', 'log2', None] + list(
+            np.arange(0.1, 1.1, 0.1)
+            ),                                                                  # Number of features to consider at every split
+        'estimator__max_leaf_nodes': [None] + list(np.linspace(
+            10, 100, num=100).astype(int)
+            ),                                                                  # How many leaf nodes can be visited
         'estimator__min_samples_split': [2, 5, 10, 15],                         # Minimum number of samples required to split a node
         'estimator__bootstrap': [True, False]                                   # Method of selecting samples for training each tree
     }
 
     # Estimator for use in random search
-    rfc = RandomForestClassifier(n_estimators=4)
+    rfc = RandomForestClassifier(n_estimators=1)
     multi_target_rfc = MultiOutputClassifier(rfc, n_jobs=-1)
 
     # Create the random search model
     rs = RandomizedSearchCV(multi_target_rfc, param_grid, n_jobs=-1, 
-                            scoring='accuracy', cv=3, 
-                            n_iter=50, verbose=1, random_state=None)
+                            scoring=avg_precision_scorer, cv=3, 
+                            n_iter=10, verbose=1, random_state=None)
 
     # Fit 
     rs.fit(X_train, y_train)
@@ -221,22 +236,21 @@ def hyperparameter_optimization(X_train, X_test, y_train, y_test):
     best_params = rs.best_params_ 
     prediction = best_model.predict(X_test)
     
-    # Calculate the accuracy score for each target
-    accuracy_scores = [accuracy_score(
-        y_test.iloc[:, i], prediction[:, i]) for i in range(y_test.shape[1])
+    # Calculate the average precision score for each target
+    precision_scores = [average_precision_score(y_test.iloc[:, i], prediction[
+        :, i]) for i in range(y_test.shape[1])
         ]
-    avg_accuracy_score = sum(accuracy_scores) / len(accuracy_scores)
-    print('Model accuracy score after hyperparameter tuning: {0:0.4f}'.format(
-        avg_accuracy_score)
+    avg_precision_score = sum(precision_scores) / len(precision_scores)
+    print('Model average precision score after hyperparameter tuning: {0:0.4f}'.format(
+        avg_precision_score)
         )
-
-    return best_model, best_params, avg_accuracy_score
+    return best_model, best_params, avg_precision_score
 
 
 def fingerprints_eval(train_data_file, test_data_file):
     """
     Returns the amount of trees that leads to the model with the highest 
-    average accuracy score.
+    average balanced accuracy score.
 
     Parameters
     ----------
@@ -249,24 +263,24 @@ def fingerprints_eval(train_data_file, test_data_file):
     -------
     best_model[3] : int
         The amount of trees leading to the model with the highest average 
-        accuracy score.
+        balanced accuracy score.
     """
     train_data = read_data(train_data_file)
     print("The amount of features is equal to {}.".format(
         len(train_data.axes[1]))
         )
     test_data = read_data(test_data_file)
-    acc_score = 0
-    for amount_trees in range(1,20):
-        y_test, y_pred, accuracy, multi_target_rfc = rf_classifier(
+    avg_precision = 0
+    for amount_trees in range(1,200):
+        y_test, y_pred, avg_precision_score, multi_target_rfc = rf_classifier(
             train_data, test_data, amount_trees
             )
         # Save the values of the best model
-        if accuracy > acc_score:
-            acc_score = accuracy
+        if avg_precision_score > avg_precision:
+            avg_precision = avg_precision_score
             best_model = [y_test, y_pred, multi_target_rfc, amount_trees]
-    print('The accuracy score of the best model is {} and it consists of {} trees.'.format(
-        acc_score, best_model[3])
+    print('The average precision score of the best model is {} and it consists of {} trees.'.format(
+        avg_precision, best_model[3])
         )
     return best_model[3]
 
@@ -283,7 +297,7 @@ def fingerprints_model(train_data_file, test_data_file, amount_trees):
     test_data_file : str
         The name of the file that contains the testing data
     amount_trees : int
-        The amount of trees used in the model
+        The amount of trees to use
     
     Returns
     -------
@@ -291,10 +305,10 @@ def fingerprints_model(train_data_file, test_data_file, amount_trees):
     """
     train_data = read_data(train_data_file)
     test_data = read_data(test_data_file)
-    y_test, y_pred, accuracy, multi_target_rfc = rf_classifier(
+    y_test, y_pred, avg_precision_score, multi_target_rfc = rf_classifier(
         train_data, test_data, amount_trees
         )
-    print('The accuracy score is {}.'.format(accuracy))
+    print('The balanced accuracy score is {}.'.format(avg_precision_score))
     create_confusion_matrix(y_test, y_pred, [
         'PKM2_inhibition', 'ERK2_inhibition']
         )
@@ -319,8 +333,8 @@ def find_best_param(train_data_file, test_data_file):
     best_params : dict
         Dictionary containing the values of the hyperparameters 
         that result in the best average accuracy score
-    accuracy : float
-        Average accuracy score over the two variables to be predicted 
+    bal_accuracy : float
+        Average balanced accuracy score over the two variables to be predicted 
     """
     train_data = read_data(train_data_file)
     X_train = train_data.drop(columns=[
@@ -338,19 +352,19 @@ def find_best_param(train_data_file, test_data_file):
         ['PKM2_inhibition', 'ERK2_inhibition']
         ]
 
-    accuracy = 0
+    avg_precision_score = 0
     # Make sure the accuracy is higher than for the default hyperparameters
-    while accuracy < 0.98:
-        best_model, best_params, accuracy = hyperparameter_optimization(
+    while avg_precision_score < 0.98:
+        best_model, best_params, avg_precision_score = hyperparameter_optimization(
             X_train, X_test, y_train, y_test
             )
     print("Best hyperparameters found: {}.".format(best_params))
-    print('The accuracy score is {}.'.format(accuracy))
+    print('The average precision score is {}.'.format(avg_precision_score))
     y_pred = best_model.predict(X_test)
     create_confusion_matrix(y_test, y_pred, 
                             ['PKM2_inhibition', 'ERK2_inhibition']
                             )
-    return best_model, best_params, accuracy
+    return best_model, best_params, avg_precision_score
 
 
     
@@ -364,20 +378,22 @@ def find_best_param(train_data_file, test_data_file):
 # r"C:\Users\20212072\OneDrive - TU Eindhoven\Documents\Year3(2023-2024)\Kwartiel4\8CC00 - Advanced programming and biomedical data analysis\Group Assignment\train_fingerprints.pkl"
 # r"C:\Users\20212072\OneDrive - TU Eindhoven\Documents\Year3(2023-2024)\Kwartiel4\8CC00 - Advanced programming and biomedical data analysis\Group Assignment\test_fingerprints.pkl"
 # input_file = 
-train_data_file = 
-test_data_file = 
+train_data_file = r"C:\Users\20212072\OneDrive - TU Eindhoven\Documents\Year3(2023-2024)\Kwartiel4\8CC00 - Advanced programming and biomedical data analysis\Group Assignment\train_fingerprints.pkl"
+test_data_file = r"C:\Users\20212072\OneDrive - TU Eindhoven\Documents\Year3(2023-2024)\Kwartiel4\8CC00 - Advanced programming and biomedical data analysis\Group Assignment\test_fingerprints.pkl"
+
+# Create a custom scorer for RandomizedSearchCV
+avg_precision_scorer = make_scorer(avg_precision, greater_is_better=True)
 
 # Find the value for the amount of trees that creates the best model:
-best_model_trees = fingerprints_eval(train_data_file, test_data_file)
+#best_model_trees = fingerprints_eval(train_data_file, test_data_file)
 
-# Create and fit the model with the determined amount of trees
-# (turns out to be 4).
-fingerprints_model(train_data_file, test_data_file, best_model_trees)
+# Create and fit the model with the determined amount of trees.
+#fingerprints_model(train_data_file, test_data_file, best_model_trees)
 
 # I tried to optimize the parameters but the accuracies never get higher 
 # than the original model with the standard parameters.
-# best_model, best_params, avg_accuracy = find_best_param(
-#     train_data_file, test_data_file)
+best_model, best_params, avg_bal_accuracy = find_best_param(
+     train_data_file, test_data_file)
 
 
 # REMARK: The model does never predict inhibition (1) for a molecule of the test set. 
