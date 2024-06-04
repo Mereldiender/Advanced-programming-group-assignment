dfb17bf8f7587c96c529f0ef145da1f6cc9fd920
VeraSneijders <99212642+VeraSneijders@users.noreply.github.com>
2024-06-04 10:43:48 +0200

    updatet

--- PCA_SVM.py
+++ PCA_SVM.py
@@ -7,9 +7,10 @@ Created on Mon Jun  3 13:41:40 2024
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-#from imblearn.over_sampling import SMOTE
 from sklearn.ensemble import RandomForestClassifier
-#TWAALF rows zijn er minder!!
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.svm import LinearSVC, SVC
 
 df_pca_incl_inh=pd.read_pickle("train_descriptors_pc_90.pkl")
 #remove smiles from datafram and the columns with the inhibition
@@ -19,8 +20,7 @@ y_pkm2 = np.array(df_pca_incl_inh.iloc[:,1])
 y_erk2 = np.array(df_pca_incl_inh.iloc[:,2])
 
 X_train, X_test, y_train, y_test = train_test_split(df_pca, y_pkm2, test_size=0.2, random_state=10)
-#%%
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
 def print_score(clf, X_train, y_train, X_test, y_test, train=True):
     if train:
         pred = clf.predict(X_train)
@@ -45,14 +45,12 @@ def print_score(clf, X_train, y_train, X_test, y_test, train=True):
         print("Confusion Matrix:")
         print(confusion_matrix(y_test, pred))
 #%%
-from sklearn.svm import LinearSVC
 model_linear = LinearSVC(loss='hinge', dual=True)
 model_linear.fit(X_train, y_train)
 print_score(model_linear, X_train, y_train, X_test, y_test, train=True)
 print_score(model_linear, X_train, y_train, X_test, y_test, train=False)
 
 #%%
-from sklearn.svm import SVC
 # The hyperparameter coef0 controls how much the model is influenced by high degree ploynomials
 model_poly = SVC(kernel='poly', degree=2, gamma='auto', coef0=1, C=5)
 model_poly.fit(X_train, y_train)
@@ -65,33 +63,20 @@ model_rbf.fit(X_train, y_train)
 
 print_score(model_rbf, X_train, y_train, X_test, y_test, train=True)
 print_score(model_rbf, X_train, y_train, X_test, y_test, train=False)
+
 #%%
-from sklearn.model_selection import GridSearchCV
-
+#penalized SVM to deal with unbalanced dataset
 param_grid = {'C': [0.01, 0.1, 0.5, 1, 10, 100], 
-              'gamma': [1, 0.75, 0.5, 0.25, 0.1, 0.01, 0.001], 
-              'kernel': ['rbf', 'poly', 'linear']} 
+              'gamma': [1, 0.75, 0.5, 0.25, 0.1, 0.01, 0.001]}
 
-grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=1, cv=5)
+
+grid = GridSearchCV(SVC(class_weight='balanced', probability=True), param_grid, refit=True, verbose=1, cv=5)
 grid.fit(X_train, y_train)
 
 best_params = grid.best_params_
 print(f"Best params: {best_params}")
 
-svm_clf = SVC(**best_params)
-svm_clf.fit(X_train, y_train)
-print_score(svm_clf, X_train, y_train, X_test, y_test, train=True)
-print_score(svm_clf, X_train, y_train, X_test, y_test, train=False)
-#%%
-from sklearn.model_selection import GridSearchCV
-param_grid = {'C': [0.01, 0.1, 0.5, 1, 10, 100], 
-              'gamma': [1, 0.75, 0.5, 0.25, 0.1, 0.01, 0.001], 
-              'kernel': ['rbf', 'poly', 'linear']} 
-grid = GridSearchCV(SVC(), param_grid, refit=True, verbose=1, cv=5, iid=True)
-grid.fit(X_train, y_train)
-best_params = grid.best_params_
-print(f"Best params: {best_params}")
-svm_clf = SVC(**best_params)
+svm_clf = SVC(class_weight='balanced', probability=True, **best_params)
 svm_clf.fit(X_train, y_train)
 print_score(svm_clf, X_train, y_train, X_test, y_test, train=True)
 print_score(svm_clf, X_train, y_train, X_test, y_test, train=False)
